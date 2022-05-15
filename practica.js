02f936aa25d3be388d0610c64829bcb4113529b3
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-15 22:53:51 +0400

    создала функцию возвращающие гласные

---
+++ practica.js
@@ -0,0 +1,13 @@
+// ------------создать функцию принимающую строку и возвращающая гласные буквы
+
+// function disemvowel(str) {
+//     let newStr = (str.replace(/A|E|I|O|U|a|e|i|o|u/g, ''))
+//     return newStr;
+//    }
+
+
+
+
+
+
+
