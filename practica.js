7b5190941c629017db438b42fd3222469b50e07b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-16 00:13:28 +0400

    Проверьте, содержит ли строка одинаковое количество «x» и «o»

--- practica.js
+++ practica.js
@@ -5,9 +5,14 @@
 //     return newStr;
 //    }
 
+// Проверьте, содержит ли строка одинаковое количество «x» и «o». Метод должен возвращать логическое значение и не учитывать регистр. Строка может содержать любой символ.
+
+function XO(str) {
+return str.toLoverCase().split('x').length === str.toLoverCase().split('o').length
+}
 
 
 
-
+ 
 
 
